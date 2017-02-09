4066e9a3ebb2f34700056d0028995ec9109fe58a
Ida <ida_sunshine@foxmail.com>
2017-02-09 16:39:52 +0800

    Initial Commit

---
+++ umengShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  umengShare
+//
+//  Created by 腾实信 on 2017/2/9.
+//  Copyright © 2017年 ida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ umengShare/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  umengShare
+//
+//  Created by 腾实信 on 2017/2/9.
+//  Copyright © 2017年 ida. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
